054437913ab2a2450b43836975ee0ad5fde7a0e9
zhu.xi <zhu.xi@immomo.com>
2020-10-27 19:43:32 +0800

    feat: ActivityIndicatorView 组件添加

---
+++ Source/ActivityIndicatorView.swift
@@ -0,0 +1,61 @@
+//
+//  ActivityIndicatorView.swift
+//  ArgoKit
+//
+//  Created by MOMO on 2020/10/27.
+//
+
+import Foundation
+
+public struct ActivityIndicatorView : View {
+    
+    private var activityIndicatorView : UIActivityIndicatorView
+    private var pNode : ArgoKitNode
+    
+    public var body: View {
+        self
+    }
+    
+    public var type: ArgoKitNodeType {
+        .single(pNode)
+    }
+    
+    public var node: ArgoKitNode? {
+        pNode
+    }
+    
+    public init(style: UIActivityIndicatorView.Style) {
+        activityIndicatorView = UIActivityIndicatorView(style: style);
+        pNode = ArgoKitNode(view: activityIndicatorView)
+        self.node?.width(point: activityIndicatorView.frame.width)
+        self.node?.height(point: activityIndicatorView.frame.height)
+    }
+}
+
+extension ActivityIndicatorView {
+    
+    public func style(_ value: UIActivityIndicatorView.Style) -> Self {
+        activityIndicatorView.style = value
+        return self
+    }
+    
+    public func hidesWhenStopped(_ value: Bool) -> Self {
+        activityIndicatorView.hidesWhenStopped = value
+        return self
+    }
+    
+    public func color(_ value: UIColor!) -> Self {
+        activityIndicatorView.color = value
+        return self
+    }
+    
+    public func startAnimating() -> Self {
+        activityIndicatorView.startAnimating()
+        return self
+    }
+    
+    public func stopAnimating() -> Self {
+        activityIndicatorView.stopAnimating()
+        return self
+    }
+}
