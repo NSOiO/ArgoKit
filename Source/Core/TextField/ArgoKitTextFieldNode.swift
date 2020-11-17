5eda0634690df47a042b8620883897417cfdd5a7
zhu.xi <zhu.xi@immomo.com>
2020-11-17 15:01:24 +0800

    feat: TextView 及 TextField 增加输出文本回调参数

--- Source/Core/TextField/ArgoKitTextFieldNode.swift
+++ Source/Core/TextField/ArgoKitTextFieldNode.swift
@@ -20,43 +20,43 @@ extension ArgoKitTextFieldNode: UITextFieldDelegate {
     
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         let sel = #selector(self.textFieldShouldBeginEditing(_:))
-        return self.sendAction(withObj: String(_sel: sel), paramter: nil) as? Bool ?? true
+        return self.sendAction(withObj: String(_sel: sel), paramter: [textField]) as? Bool ?? true
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
         let sel = #selector(self.textFieldDidBeginEditing(_:))
-        self.sendAction(withObj: String(_sel: sel), paramter: nil)
+        self.sendAction(withObj: String(_sel: sel), paramter: [textField])
     }
     
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
         let sel = #selector(self.textFieldShouldEndEditing(_:))
-        return self.sendAction(withObj: String(_sel: sel), paramter: nil) as? Bool ?? true
+        return self.sendAction(withObj: String(_sel: sel), paramter: [textField]) as? Bool ?? true
     }
     
     @available(iOS 10.0, *)
     func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
         let sel = #selector(self.textFieldDidEndEditing(_:reason:))
-        self.sendAction(withObj: String(_sel: sel), paramter: [reason])
+        self.sendAction(withObj: String(_sel: sel), paramter: [textField, reason])
     }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let sel = #selector(self.textField(_:shouldChangeCharactersIn:replacementString:))
-        return self.sendAction(withObj: String(_sel: sel), paramter: [range, string]) as? Bool ?? true
+        return self.sendAction(withObj: String(_sel: sel), paramter: [textField, range, string]) as? Bool ?? true
     }
     
     @available(iOS 13.0, *)
     func textFieldDidChangeSelection(_ textField: UITextField) {
         let sel = #selector(self.textFieldDidChangeSelection(_:))
-        self.sendAction(withObj: String(_sel: sel), paramter: nil)
+        self.sendAction(withObj: String(_sel: sel), paramter: [textField])
     }
         
     func textFieldShouldClear(_ textField: UITextField) -> Bool {
         let sel = #selector(self.textFieldShouldClear(_:))
-        return self.sendAction(withObj: String(_sel: sel), paramter: nil) as? Bool ?? true
+        return self.sendAction(withObj: String(_sel: sel), paramter: [textField]) as? Bool ?? true
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         let sel = #selector(self.textFieldShouldReturn(_:))
-        return self.sendAction(withObj: String(_sel: sel), paramter: nil) as? Bool ?? true
+        return self.sendAction(withObj: String(_sel: sel), paramter: [textField]) as? Bool ?? true
     }
 }
