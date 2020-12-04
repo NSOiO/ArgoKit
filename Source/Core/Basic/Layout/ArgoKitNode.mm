b8a4695219484e03a9229113525e409df55972ba
zhu.xi <zhu.xi@immomo.com>
2020-12-04 14:16:41 +0800

    Revert "test: 临时注释代码 需还原"
    This reverts commit 0bd0aa0b7b137fabccbcc28a4cb8e13a4eabd66b.

--- Source/Core/Basic/Layout/ArgoKitNode.mm
+++ Source/Core/Basic/Layout/ArgoKitNode.mm
@@ -13,7 +13,7 @@
 #import "ArgoKitNodeViewModifier.h"
 #import "ArgoKitNode+Frame.h"
 #import "ArgoKitNode+Observer.h"
-//#import "UIView+AKFrame.h"
+#import "UIView+AKFrame.h"
 
 @implementation NodeAction
 - (instancetype)initWithAction:(ArgoKitNodeBlock)action controlEvents:(UIControlEvents)controlEvents{
@@ -372,7 +372,7 @@ static CGFloat YGRoundPixelValue(CGFloat value)
 
 - (UIView *)createNodeViewWithFrame:(CGRect)frame {
     UIView *view = [self.viewClass new];
-    view.frame = frame;
+    view.akLayoutFrame = frame;
     return view;
 }
 
@@ -391,8 +391,8 @@ static CGFloat YGRoundPixelValue(CGFloat value)
                     observer.createViewBlock(wealSelf.view);
                 }
             }
-        }else if (!CGRectEqualToRect(frame, wealSelf.view.frame)) {
-            wealSelf.view.frame = frame;
+        }else if (!CGRectEqualToRect(frame, wealSelf.view.akLayoutFrame)) {
+            wealSelf.view.akLayoutFrame = frame;
             if (!wealSelf.view.superview) {
                 [wealSelf insertViewToParentNodeView];
             }
