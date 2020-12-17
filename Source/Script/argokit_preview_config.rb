8eafad86767f80df0ea50dfea4c376ce5859c4ac
dai.dongpeng <swiftwithlove@163.com>
2020-12-17 12:34:33 +0800

    [Preview] 修改rb脚本

--- Source/Script/argokit_preview_config.rb
+++ Source/Script/argokit_preview_config.rb
@@ -1,4 +1,81 @@
+
+#Usage:
+# 将下面的代码拷贝到Podfile中即可
+#post_integrate do |installer|
+#    installer.generated_pod_targets.each do |target|
+#      if target.name == "ArgoKitPreview"
+#        pod_dir = target.sandbox.pod_dir(target.name)
+#        require "#{pod_dir}/Source/Script/argokit_preview_config.rb"
+#        config_preview_files installer
+#      end
+#    end
+#end
+
 require "xcodeproj"
+
+def config_preview_files(installer, remove_if_exist = false, ap_target_name = "ArgoKitPreview")
+  ap_files = ["/Source/Preview/ArgoKitPreviewTypes.swift"]
+  file_paths = Array.new
+  
+  installer.aggregate_targets.each do | target |
+    ap_pod = target.pod_targets.select {|item| item.name == ap_target_name }.first
+    if ap_pod
+      ap_pod_dir = ap_pod.sandbox.pod_dir(ap_pod.name)
+      ap_files.each do | file |
+        file_paths << "#{ap_pod_dir}#{file}"
+      end
+      add_preview_files target, ap_pod, file_paths, remove_if_exist
+    end
+  end
+end
+
+def add_preview_files(agg_pod, ap_pod, file_paths, remove_if_exist)
+  ap_group = 'ArgoKitPreviewFiles'
+  argokit_prefix = "[ArgoKitPreview]"
+  project = agg_pod.user_project
+  
+  user_targets = Array.new
+  agg_pod.user_target_uuids.each do |uuid|
+    t = project.objects_by_uuid[uuid]
+    t ? user_targets << t : nil
+  end
+  
+  if user_targets.length == 0
+    puts "#{argokit_prefix} Can't find user targets".red
+    return
+  end
+  
+  group = project.main_group.find_subpath(File.join(ap_group), true)
+  
+  file_paths.each do |file_path|
+      file_name = File.basename(file_path)
+      file_exist = false
+      group.children.each do |child|
+          if child.name == file_name
+              if remove_if_exist
+                  child.remove_from_project()
+                  puts "#{argokit_prefix} file #{file_name} exist, remove from project.".green
+              else
+                  file_exist = true
+                  puts "#{argokit_prefix} file #{file_name} exist, no longer need to add.".green
+              end
+          end
+      end# group.children
+      
+      if !file_exist
+          file_ref = group.new_reference(file_path, :absolute)
+          user_targets.each do |target|
+            ret = target.add_file_references([file_ref])
+            if ret
+                puts "#{argokit_prefix} succ to add  #{file_name} to target #{target.name}".green
+            else
+                puts "#{argokit_prefix} failed to add  #{file_name} to target #{target.name}".red
+            end
+          end
+      end# !file_exist
+  end
+end
+
 
 class String
     def black;          "\e[30m#{self}\e[0m" end
@@ -26,85 +103,70 @@ class String
     def reverse_color;  "\e[7m#{self}\e[27m" end
 end
 
-def integrate_argokit_preview(installer, main_project ,main_target, remove_if_exist = true, ap_target = "ArgoKitPreview")
-    argokit_prefix = "[ArgoKitPreview]"
-    puts "#{argokit_prefix} Start Integrating ArgoKitPreview"
-    
-    ap_files = ["/Source/Preview/ArgoKitPreviewTypes.swift"]
-    ap_group = 'ArgoKitPreviewFiles'
-    
-    #  ap_group = 'ArgoKitPreview'
-        
-    file_paths = Array.new
-    installer.generated_pod_targets.each do |target|
-        if target.name == ap_target
-            pod_dir = target.sandbox.pod_dir(target.name)
-            ap_files.each do |file|
-                file_paths <<  "#{pod_dir}/Source/Preview/ArgoKitPreviewTypes.swift"
-            end
-        end
-    end
 
-    if file_paths.length <= 0
-        puts "#{argokit_prefix} There's nothing to deal with.".red
-        return
-    end
-
-    project_path = Dir.pwd + "/#{main_project}.xcodeproj"
-    # puts project_path
-
-    project = Xcodeproj::Project.open(project_path)
-    project.targets.each do |target|
-        if target.name == main_target
-            # puts target.name
-            group = project.main_group.find_subpath(File.join(ap_group), true)
-            file_paths.each do |file_path|
-                file_name = File.basename(file_path)
-                file_exist = false
-
-                group.children.each do |child|
-                    if child.name == file_name
-                        if remove_if_exist
-                            child.remove_from_project()
-                            puts "#{argokit_prefix} file #{file_name} exist, remove from project.".green
-                        else
-                            file_exist = true
-                            puts "#{argokit_prefix} file #{file_name} exist, no longer need to add.".green
-                        end
-                        break
-                    end
-                end
-
-                if !file_exist
-                    file_ref = group.new_reference(file_path, :absolute)
-                    ret = target.add_file_references([file_ref])
-                    if ret
-                        puts "#{argokit_prefix} succ to add  #{file_name} to #{ap_group}".green
-                    else
-                        puts "#{argokit_prefix} failed to add  #{file_name}".red
-                    end
-                end# !file_exist
-            end# file_paths.each
-        end# if target.name
-    end# project.targets.each
-    
-    project.save
-    puts "#{argokit_prefix} End Integrating ArgoKitPreview"
-end
-
-
-#Usage:
-# 需要将main_project和main_target替换成真实数据
-#post_integrate do |installer|
-#    main_project = '*******'
-#    main_target = "*******"
+#def integrate_argokit_preview(installer, main_project ,main_target, remove_if_exist = true, ap_target = "ArgoKitPreview")
+#    argokit_prefix = "[ArgoKitPreview]"
+#    puts "#{argokit_prefix} Start Integrating ArgoKitPreview"
 #
+#    ap_files = ["/Source/Preview/ArgoKitPreviewTypes.swift"]
+#    ap_group = 'ArgoKitPreviewFiles'
+#
+#    #  ap_group = 'ArgoKitPreview'
+#
+#    file_paths = Array.new
 #    installer.generated_pod_targets.each do |target|
-#      if target.name == "ArgoKitPreview"
-#        pod_dir = target.sandbox.pod_dir(target.name)
-#        file_path = "#{pod_dir}/Source/Script/argokit_preview_config.rb"
-#        require file_path
-#        integrate_argokit_preview installer, main_project, main_target
-#      end
+#        if target.name == ap_target
+#            pod_dir = target.sandbox.pod_dir(target.name)
+#            ap_files.each do |file|
+#                file_paths <<  "#{pod_dir}/Source/Preview/ArgoKitPreviewTypes.swift"
+#            end
+#        end
 #    end
+#
+#    if file_paths.length <= 0
+#        puts "#{argokit_prefix} There's nothing to deal with.".red
+#        return
+#    end
+#
+#    project_path = Dir.pwd + "/#{main_project}.xcodeproj"
+#    # puts project_path
+#
+#    project = Xcodeproj::Project.open(project_path)
+#    project.targets.each do |target|
+#        if target.name == main_target
+#            # puts target.name
+#            group = project.main_group.find_subpath(File.join(ap_group), true)
+#            file_paths.each do |file_path|
+#                file_name = File.basename(file_path)
+#                file_exist = false
+#
+#                group.children.each do |child|
+#                    if child.name == file_name
+#                        if remove_if_exist
+#                            child.remove_from_project()
+#                            puts "#{argokit_prefix} file #{file_name} exist, remove from project.".green
+#                        else
+#                            file_exist = true
+#                            puts "#{argokit_prefix} file #{file_name} exist, no longer need to add.".green
+#                        end
+#                        break
+#                    end
+#                end
+#
+#                if !file_exist
+#                    file_ref = group.new_reference(file_path, :absolute)
+#                    ret = target.add_file_references([file_ref])
+#                    if ret
+#                        puts "#{argokit_prefix} succ to add  #{file_name} to #{ap_group}".green
+#                    else
+#                        puts "#{argokit_prefix} failed to add  #{file_name}".red
+#                    end
+#                end# !file_exist
+#            end# file_paths.each
+#        end# if target.name
+#    end# project.targets.each
+#
+#    project.save
+#    puts "#{argokit_prefix} End Integrating ArgoKitPreview"
 #end
+
