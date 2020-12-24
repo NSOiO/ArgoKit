acd51dc22fe14282105d64f783073b028b7846fc
zhu.xi <zhu.xi@immomo.com>
2020-12-24 15:17:37 +0800

    docs: AlertView docs

--- Source/Core/Component/AlertView.swift
+++ Source/Core/Component/AlertView.swift
@@ -6,33 +6,51 @@
 //
 
 import Foundation
-@objcMembers class ArgokitAlertViewNode:ArgoKitNode{
+
+@objcMembers class ArgokitAlertViewNode: ArgoKitNode {
     public dynamic var isPresented: Bool = false
-    private var alertController:UIAlertController
+    private var alertController: UIAlertController
     var observer: NSKeyValueObservation!
-    public init(alertController:UIAlertController) {
+    
+    public init(alertController: UIAlertController) {
         self.alertController = alertController
         super.init(viewClass: UIView.self)
     }
 }
+
+/// Wrapper of UIAlertController
+/// An object that displays an alert message to the user.
 public class AlertView: View {
-    let pNode:ArgokitAlertViewNode
-    let alerView:UIAlertController
-    var pTextField:UITextField?
-    public var node: ArgoKitNode?{
+    let pNode: ArgokitAlertViewNode
+    let alerView: UIAlertController
+    var pTextField: UITextField?
+    
+    public var node: ArgoKitNode? {
         pNode
     }
-    public init(title: String? = "", message: String? = "", preferredStyle: UIAlertController.Style){
+    
+    /// Initializer
+    /// - Parameters:
+    ///   - title: The title of the alert. Use this string to get the user’s attention and communicate the reason for the alert.
+    ///   - message: Descriptive text that provides additional details about the reason for the alert.
+    ///   - preferredStyle: The style to use when presenting the alert controller. Use this parameter to configure the alert controller as an action sheet or as a modal alert.
+    public init(title: String? = "", message: String? = "", preferredStyle: UIAlertController.Style) {
         alerView = UIAlertController(title: title, message: message, preferredStyle: preferredStyle)
         pNode = ArgokitAlertViewNode(alertController: alerView)
     }
 }
-extension AlertView{
+
+extension AlertView {
     
+    /// Add default style Action.
+    /// - Parameters:
+    ///   - title: The text to use for the button title. The value you specify should be localized for the user’s current language.
+    ///   - handler: A block to execute when the user selects the action. This block has no return value and takes the selected action object as its only parameter.
+    /// - Returns: Self
     @discardableResult
-    public func `default`(title:String?,handler:((String?)->Void)?)->Self{
-        let alertAction:UIAlertAction = UIAlertAction(title: title, style: UIAlertAction.Style.default) {[weak self] action in
-            if let block = handler{
+    public func `default`(title: String?, handler: ((String?) -> Void)?) -> Self {
+        let alertAction:UIAlertAction = UIAlertAction(title: title, style: UIAlertAction.Style.default) { [weak self] action in
+            if let block = handler {
                 block(self?.pTextField?.text)
             }
         }
@@ -40,22 +58,31 @@ extension AlertView{
         return self
     }
     
+    /// Add cancel style Action.
+    /// - Parameters:
+    ///   - title: The text to use for the button title. The value you specify should be localized for the user’s current language.
+    ///   - handler: A block to execute when the user selects the action. This block has no return value and takes the selected action object as its only parameter.
+    /// - Returns: Self
     @discardableResult
-    public func cancel(title:String?,handler:(()->Void)?)->Self{
+    public func cancel(title: String?, handler: (() -> Void)?) -> Self {
         let alertAction:UIAlertAction = UIAlertAction(title: title, style: UIAlertAction.Style.cancel) { action in
-            if let block = handler{
+            if let block = handler {
                 block()
             }
         }
         alerView.addAction(alertAction)
-        
         return self
     }
     
+    /// Add destructive style Action.
+    /// - Parameters:
+    ///   - title: The text to use for the button title. The value you specify should be localized for the user’s current language.
+    ///   - handler: A block to execute when the user selects the action. This block has no return value and takes the selected action object as its only parameter.
+    /// - Returns: Self
     @discardableResult
-    public func destructive(title:String?,handler:((String?)->Void)?)->Self{
+    public func destructive(title: String?, handler: ((String?) -> Void)?) -> Self {
         let alertAction:UIAlertAction = UIAlertAction(title: title, style: UIAlertAction.Style.destructive) { [weak self] action in
-            if let block = handler{
+            if let block = handler {
                 block(self?.pTextField?.text)
             }
         }
@@ -63,16 +90,19 @@ extension AlertView{
         return self
     }
     
+    /// Add TextField.
+    /// - Returns: Self
     @discardableResult
-    public func textField()->Self{
+    public func textField() -> Self {
         alerView.addTextField { [weak self] textFiled in
             self?.pTextField = textFiled
         }
         return self
     }
     
-    public func show(){
-        if let viewController = self.rootViewController(){
+    /// Present alert view
+    public func show() {
+        if let viewController = self.rootViewController() {
             viewController.present(alerView, animated: true, completion: nil)
         }
     }
@@ -84,22 +114,32 @@ open var preferredAction: UIAlertAction?
 
 open func addTextField(configurationHandler: ((UITextField) -> Void)? = nil)
  */
-extension AlertView{
+extension AlertView {
     
+    /// Set the title of the alert.
+    /// - Parameter value: The title of the alert. Use this string to get the user’s attention and communicate the reason for the alert.
+    /// - Returns: Self
     @discardableResult
-    public func titile(_ value: String?)->Self{
+    public func titile(_ value: String?) -> Self {
         alerView.title = value
         return self
     }
     
+    /// Set descriptive text that provides additional details about the reason for the alert.
+    /// - Parameter value: Descriptive text that provides additional details about the reason for the alert.
+    /// - Returns: Self
     @discardableResult
-    public func message(_ value: String?)->Self{
+    public func message(_ value: String?) -> Self {
         alerView.message = value
         return self
     }
 }
 
-extension View{
+extension View {
+    
+    /// Add alert View
+    /// - Parameter content: the builder of alert View
+    /// - Returns: Self
     @discardableResult
     public func alert(_ content:()->AlertView) -> Self{
         let alertView = content()
