a430e4eb6e46114027323ba40b8e149982fb27e4
dai.dongpeng <swiftwithlove@163.com>
2021-01-25 11:35:28 +0800

    [ArgoKit] change image loader

--- Source/Component/KFImageLoader/KFImageLoader.swift
+++ Source/Component/KFImageLoader/KFImageLoader.swift
@@ -3,10 +3,7 @@ import UIKit
 import ArgoKit
 import Kingfisher
 
-#if !SDIMAGELOADER
 public typealias ImageLoader = KFImageLoader
-#endif
-
 public class KFImageLoader:ArgoKitImageLoader {
     public init () {}
     public func loadImage(url: URL?, successed: ((UIImage) -> ())?, failure: ((Error?) -> ())?) {
