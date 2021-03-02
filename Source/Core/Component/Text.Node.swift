ddda10bc8fbe117a8f420807ed18ebe446384167
Bruce <zhang.shuisheng@immomo.com>
2021-03-02 12:17:04 +0800

    文本处理

--- Source/Core/Component/Text.Node.swift
+++ Source/Core/Component/Text.Node.swift
@@ -241,6 +241,14 @@ open class ArgoKitTextBaseNode: ArgoKitArttibuteNode{
         var result = size
         var maxHeight = size.height
         if let attribut = self.attributedText{
+            attribut.enumerateAttributes(in: NSRange(location: 0, length: attribut.length), options: NSAttributedString.EnumerationOptions(rawValue: 0)) {[weak self] (attrs, range, stop) in
+                            if attrs[NSAttributedString.Key.font] == nil{
+                                if let `self` = self{
+                                    self.font(self.font,range: range)
+                                }
+                            }
+                
+                        }
             var totolLineHeight:CGFloat = 0
             if numberOfLines > 0 {
                 let lineHeight:CGFloat = ceil(font.lineHeight + font.leading)
